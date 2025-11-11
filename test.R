bb54086ea2652e39ae245dcad7e76af0bf7b52e4
esmoreido <moreido@mail.ru>
2025-11-11 17:31:47 +0300

    update rds

--- test.R
+++ test.R
@@ -22,11 +22,9 @@ url <- 'https://docs.google.com/spreadsheets/d/1P0yhwLmuskqrG0KR0fHpu3VwIyGSyeKI
 sn <- sheet_names(url)
 
 # все данные в один список
-# gs <- lapply(sn, read_sheet, ss = url, na = c('-'))
-# names(gs) <- sn
-# save(gs, file = 'google_data.RData')
-# load('google_data.RData')
-# saveRDS(gs, file = 'google_data.rds')
+gs <- lapply(sn, read_sheet, ss = url, na = c('-'))
+names(gs) <- sn
+saveRDS(gs, file = 'google_data.rds')
 gs <- readRDS('google_data.rds')
 # статистика по наполнению
 stat_stat <- data.frame(stat = gsub(".\\..", "", sn), nsample = sapply(gs, nrow))
